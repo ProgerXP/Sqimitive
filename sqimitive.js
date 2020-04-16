a07c1fb79753d109d2354e7a5f2e7e90b9043b74
Proger_XP <proger.xp@gmail.com>
2020-04-16 16:57:44 +0300

    Replaced sqimitive.js wrapper with Universal Module Header
    Potentially breaking: window.Sqimitive is replaced entirely, no longer merged if predefined before script inclusion.

--- sqimitive.js
+++ sqimitive.js
@@ -25,18 +25,43 @@
 
  ***/
 
-// This fancy construct prepares the library for various module frameworks.
-;(function (root, init) {
-  if (typeof define == 'function' && define.amd) {
-    define(['exports', 'underscore', 'jquery'], function (exports, _, $) {
-      init(exports, _, $)
-    })
-  } else if (typeof exports !== 'undefined') {
-    init(exports, require('underscore'), require('jquery'))
+;(function (factory) {
+  var deps = {underscore: '_', jquery: '$'}
+  var me = 'Sqimitive'
+  // --- Universal Module (squizzle.me) - CommonJS - AMD - window --- IE9+ ---
+
+  if (typeof exports != 'undefined' && !exports.nodeType) {
+    // CommonJS/Node.js.
+    deps = Object.keys(deps).map(function (dep) { return require(dep) })
+    if (typeof module != 'undefined' && module.exports) {
+      module.exports = factory.apply(this, deps)
+    } else {
+      exports = factory.apply(this, deps)
+    }
+  } else if (typeof define == 'function' && define.amd) {
+    // AMD/Require.js.
+    define(Object.keys(deps), factory)
   } else {
-    root.Sqimitive = _.extend(root.Sqimitive || {}, init(root._, root.$))
+    // In-browser. self = window or web worker scope.
+    var root = typeof self == 'object' ? self
+             : typeof global == 'object' ? global
+             : (this || {})
+    var by = function (obj, path) {
+      path = path.split(/\./g)
+      while (obj && path.length) { obj = obj[path.shift()] }
+      return obj
+    }
+    // No Object.values() in IE.
+    deps = Object.keys(deps).map(function (dep) {
+      var res = by(root, deps[dep])
+      if (!res) { throw me + ': missing dependency: ' + dep }
+      return res
+    })
+    me = me.split(/\.([^.]+)$/)
+    if (me.length > 1) { root = by(root, me.shift()) }
+    root[me[0]] = factory.apply(this, deps)
   }
-})(this, function (_, $) {
+}).call(this, function (_, $) {
   // Subclass extension method, taken from Backbone.
   var extend = function (protoProps, staticProps) {
     var parent = this
